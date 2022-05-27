c9581f9993d67a99d5bbf78eb533327f3d75ab50
Besermenji <besermenji.vladimir@gmail.com>
2022-05-27 16:56:54 +0200

    Build updated to project.

--- android/src/main/java/com/besermenji/safetynet/RNSafetyNetPackage.java
+++ android/src/main/java/com/besermenji/safetynet/RNSafetyNetPackage.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -14,6 +15,11 @@ public class RNSafetyNetPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNSafetyNetModule(reactContext));
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return null;
     }
 
     @Override
